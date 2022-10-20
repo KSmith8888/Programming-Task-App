b583e3f9b01bd7e03c589424699cf7f9787a74c4
KSmith8888 <83607802+KSmith8888@users.noreply.github.com>
2022-10-19 17:53:06 -0700

    Error handling test

--- sw.js
+++ sw.js
@@ -16,47 +16,46 @@ function initializeWorker() {
   ];
 
   self.addEventListener('install', (event) => {
-      try {
-        event.waitUntil((async () => {
-          const cache = await caches.open(cacheName);
-          await cache.addAll(appFiles);
-        })());
-      } catch(error) {
-        console.error(`Service worker installation or cache error: ${error}`);
+      async function cacheFiles() {
+        const cache = await caches.open(cacheName);
+        await cache.addAll(appFiles);
       }
+      event.waitUntil(cacheFiles().catch((error) => {
+        console.error(`Service worker installation or cache error: ${error}`);
+    }));
   });
 
   self.addEventListener('fetch', (event) => {
-    try {
-      let { request } = event;
-      console.log(event);
+      let request = event.request;
       //Remove range header from audio file to avoid cache issue
+      /*
       if (request.headers.has('range')) {
         const newHeaders = new Headers(request.headers);
         newHeaders.delete('range');
-        request = new Request(request.url, {
-            body: request.body,
-            method: request.method,
-            mode: request.mode,
-            headers: newHeaders
+        request = new Request(
+          request.url, { 
+          body: request.body,
+          method: request.method,
+          mode: request.mode,
+          headers: newHeaders
         });
-        console.log('NEW', event);
       }
-      event.respondWith((async () => {
+      */
+      async function getFiles() {
         const cacheResponse = await caches.match(request);
-          console.log(`[Service Worker] Fetching resources from cache: ${request.url}`);
+        console.log(`Service Worker fetching resources from cache: ${request.url}`);
         if (cacheResponse) { 
           return cacheResponse; 
         }
         const networkResponse = await fetch(request);
         const cache = await caches.open(cacheName);
-          console.log(`[Service Worker] Caching new resource from the network: ${request.url}`);
+        console.log(`Service Worker caching new resource from the network: ${request.url}`);
         cache.put(request, networkResponse.clone());
         return networkResponse;
-    })());
-    } catch(error) {
+      }
+      event.respondWith(getFiles().catch((error) => {
       console.error(`Service worker fetch error: ${error}`);
-    }
+      }));
   });
 }
 
